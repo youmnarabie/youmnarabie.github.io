aa991966c793c00db4fd3d5b366a2412e161a956
Youmna Rabie <rabie.youmna@gmail.com>
2023-07-03 22:59:44 -0700

    Navbar

---
+++ app.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLink = document.querySelector('.navbar__menu')
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active')
+    menuLink.classList.toggle('active')
+})
